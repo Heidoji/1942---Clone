592ac4fcd133fa54a8c694af70052b5e6e94e902
Heidoji <heidoji@tutanota.com>
2016-08-21 21:28:06 +0200

    Version 0.0.5 : Add Bonus and prepare screen for start and Game-over

--- main.lua
+++ main.lua
@@ -1,11 +1,7 @@
--- charger les tiles du fond
 -- cherger les images des ennemis
--- charger les sons
 -- faire un niveau
--- gerer les tirz
 -- gerer les bonus
 -- afficher un ecran d'accueil
--- gerer le score
 -- gerer energie player
 -- gerer les ennemis
 
@@ -57,19 +53,13 @@ score_l = imgScore:getWidth()
 sonTirPlayer = love.audio.newSource("sons/sonTirPlayer.wav", "static")
 sonExplode = love.audio.newSource("sons/explode_touch.wav", "static")
 
--- Création des listes
-liste_sprites = {}
-liste_tirs = {}
-liste_enemies = {}
+
 
 -- Création Joueur
 player = {}
 
 -- Création caméra
 camera = {}
-camera.x = 0
-camera.y = 1
-camera.vy = 1
 
 -- Chargement niveau
 map = require("map")
@@ -168,7 +158,7 @@ function CreateEnemy(pType, pX, pY)
   enemy.type = pType
   enemy.supprime = false
   enemy.endormi = true
-  enemy.chronotir = 0
+  enemy.count = false
   
   if pType == 1 then
     enemy.vx = 0
@@ -187,11 +177,70 @@ function CreateEnemy(pType, pX, pY)
     enemy.vy = 3
     enemy.energie = 3
     enemy.score = 100
-    enemy.frequence_tir = 400
+    enemy.frequence_tir = 1000
+  elseif pType == 4 then
+    enemy.vx = 2
+    enemy.vy = 1
+    enemy.energie = 5
+    enemy.score = 150
+    enemy.frequence_tir = 80
+  elseif pType == 5 then
+    enemy.vx = -2
+    enemy.vy = 1
+    enemy.energie = 5
+    enemy.score = 150
+    enemy.frequence_tir = 80
   end
+  
+  enemy.chronotir = math.random(enemy.frequence_tir / 5, enemy.frequence_tir / 2)
   
   table.insert(liste_enemies, enemy)
   
+end
+
+function CreateBonus(pX, pY)
+  bonus = CreateSprite("bonus_weapon", pX, pY)
+  bonus.x = pX
+  bonus.y = pY
+  bonus.timelapse = 240
+  bonus.supprime = false
+
+  table.insert(liste_bonus, bonus)
+end
+
+function DemarreJeu()
+  -- Création des listes
+  liste_sprites = {}
+  liste_tirs = {}
+  liste_enemies = {}
+  liste_bonus = {}
+  
+  camera.x = 0
+  camera.y = 1
+  camera.vy = 1
+  
+  player = CreateSprite("player", largeur / 2, hauteur /2)
+  player.y = hauteur - player.h - HAUTEUR_SCORE
+  player.tir = 1
+  player.life = 3
+  player.score = 0
+  player.bonus = 0
+  player.ennemi = 0
+  player.abattu = 0
+  player.timelapse = 0
+  
+  -- Chargement ennemis
+  local n
+  local firstGID = map.tilesets[2].firstgid
+  local ennemies = map.layers[2].objects
+  
+  for n = 1, #ennemies do
+    local o = ennemies[n]
+    local typeEnnemy = (o.gid - firstGID) + 1
+    local posX = o.x + 32/2
+    local posY = - (map.height * map.tileheight - 700) + o.y - 32 / 2
+    CreateEnemy(typeEnnemy, posX, posY)
+  end
 end
 
 function love.load()
@@ -201,33 +250,20 @@ function love.load()
   largeur = love.graphics.getWidth()
   hauteur = love.graphics.getHeight()
   
-  player = CreateSprite("player", largeur / 2, hauteur /2)
-  player.y = hauteur - player.h - HAUTEUR_SCORE
-  player.tir = 1
-  player.life = 3
-  player.score = 0
-  
-  local n, m
-  
-  for n=0, 9 do
-    CreateEnemy(1, largeur / 2 - n * 40, - n * 40)
-    --CreateEnemy(1, largeur / 2 + n * 40, - 400 - n * 40)
-    --CreateEnemy(2, largeur / 2 + n * 40, - 500 - n * 20)
-    --CreateEnemy(2, largeur / 2 - n * 40, - 900 - n * 20)
-  end
-  
-  --CreateEnemy(3, largeur / 2, 0)
-  
+  menu = "Accueil"
 end
 
 function love.update(dt)
-  update_enemies()
-  update_tirs()
-  update_sprites()
-  update_clavier()
+  if menu == "Jeu" then
+    update_enemies()
+    update_player_tir()
+    update_bonus()
+    update_sprites()
+    update_clavier()
+  end
 end
 
-function update_tirs()
+function update_player_tir()
   local n
   local nEnemies
   
@@ -238,8 +274,15 @@ function update_tirs()
     
     if tir.type == "enemy" then
       if collide(player, tir, 32, 32) then
-        print("Boom, touché")
-        player.life = player.life - 1
+        if player.tir > 1 then
+          player.tir = player.tir - 1
+        else
+          if player.life == 0 then
+            menu = "Game Over"
+          else
+            player.life = player.life - 1
+          end
+        end
         tir.supprime = true
         table.remove(liste_tirs, n)
       end
@@ -263,6 +306,14 @@ function update_tirs()
             end
             
             player.score = player.score + enemy.score
+            player.abattu = player.abattu + 1
+            player.timelapse = player.timelapse + 1
+            
+            if player.timelapse % 10 == 0 and math.random(0, 40) <= player.timelapse * (player.abattu / player.ennemi) then
+              CreateBonus(enemy.x, enemy.y)
+              player.timelapse = 0
+            end
+            
             table.remove(liste_enemies, nEnemies)
           end
         end
@@ -284,6 +335,11 @@ function update_enemies()
     local vx, vy
     local angle
     
+    if enemy.y >= -25 and enemy.count == false then
+      enemy.count = true
+      player.ennemi = player.ennemi + 1
+    end
+    
     if enemy.y >= -10 then
       enemy.endormi = false
     end
@@ -303,11 +359,17 @@ function update_enemies()
       
       enemy.chronotir = enemy.chronotir - 1
 
-      update_enemies_tir(enemy, vx, vy)
+      update_tir_timer(enemy, vx, vy)
       
       if collide(player, enemy, 32, 32) then
         CreateExplosion(enemy.x, enemy.y)
-        player.life = player.life - 1
+        
+        if player.life == 0 then
+          menu = "Game Over"
+        else
+          player.life = player.life - 1
+        end
+  
         enemy.energie = enemy.energie - 1
         if enemy.energie <= 0 then
           local nExplosion
@@ -332,11 +394,33 @@ function update_enemies()
   end
 end
 
-function update_enemies_tir(pEnemy, pVx, pVy)
+function update_tir_timer(pEnemy, pVx, pVy)
   if pEnemy.chronotir <= 0 then
     pEnemy.chronotir = math.random(enemy.frequence_tir- 25, enemy.frequence_tir + 25)
     CreateTir("enemy", "tir_enemy", pEnemy.x, pEnemy.y, pVx, pVy)
   end
+end
+
+
+function update_bonus()    
+    for n=#liste_bonus, 1, -1 do
+      local bonus = liste_bonus[n]
+    
+      bonus.timelapse = bonus.timelapse - 1 
+            
+      if collide(player, bonus, 32, 32) then
+        player.tir = player.tir + 1
+        bonus.supprime = true
+        table.remove(liste_bonus, n)
+      end
+      
+      if bonus.timelapse <= 0 then
+        bonus.supprime = true
+        table.remove(liste_bonus, n)
+      end
+      
+
+    end
 end
 
 function update_sprites()
@@ -381,66 +465,81 @@ function update_clavier()
 end
 
 function love.draw()
-  local nbLignes = map.layers[1].height
-  local ligne, colonne
-  local x, y
-  
-  y = hauteur + camera.y
-  x = 0
-  camera.y = camera.y + camera.vy
-  
-  for ligne = nbLignes, 1, -1 do
-    for colonne=1, map.layers[1].width do
-      --Dessine la tuile
-      local tuile = niveau[((ligne - 1) * map.layers[1].width) + colonne ]
-      if tuile > 0 then
-        love.graphics.draw(imgTuiles[tuile], x, y)
-      end
-      x = x + 32
-    end
+  if menu == "Jeu" then
+    local nbLignes = map.layers[1].height
+    local ligne, colonne
+    local x, y
+    
+    y = hauteur + camera.y
     x = 0
-    y = y - 32
-  end
-  
-  local n
-  local score = {}
+    camera.y = camera.y + camera.vy
+    
+    for ligne = nbLignes, 1, -1 do
+      for colonne=1, map.layers[1].width do
+        --Dessine la tuile
+        local tuile = niveau[((ligne - 1) * map.layers[1].width) + colonne ]
+        if tuile > 0 then
+          love.graphics.draw(imgTuiles[tuile], x, y)
+        end
+        x = x + 32
+      end
+      x = 0
+      y = y - 32
+    end
+    
+    local n
+    local score = {}
 
-  for n=1, #liste_sprites do
-    local s = liste_sprites[n]
-    love.graphics.draw(s.image, s.x + camera.x, s.y, 0, 1, 1, s.l / 2, s.h / 2)
+    for n=1, #liste_sprites do
+      local s = liste_sprites[n]
+      love.graphics.draw(s.image, s.x + camera.x, s.y, 0, 1, 1, s.l / 2, s.h / 2)
+    end
+    
+    love.graphics.print("Total Ennemis : "..player.ennemi.." Total Abattu : "..player.abattu.." score : "..player.score, 0, 0)
+    love.graphics.draw(imgLifes[player.life], 5, hauteur - 50)
+    love.graphics.draw(imgScore, largeur - score_l - 5, hauteur - 50)
+    
+    score = CalculScore(player.score)
+    
+    for n=1, #score do
+      love.graphics.draw(imgChiffres[score[n]], largeur - 14 - n * 11, hauteur - 41)
+    end
+  elseif menu == "Accueil" then
+    love.graphics.print("1942 - Appuyer sur Espace", largeur / 2, hauteur / 2)
+  elseif menu == "Game Over" then
+    love.graphics.print("Game Over - Appuyer sur Entrée", largeur / 2, hauteur / 2)
   end
-  
-  love.graphics.print("Total Sprite : "..#liste_sprites.." Total Tir : "..#liste_tirs.." score : "..player.score, 0, 0)
-  love.graphics.draw(imgLifes[player.life], 5, hauteur - 50)
-  love.graphics.draw(imgScore, largeur - score_l - 5, hauteur - 50)
-  
-  score = CalculScore(player.score)
-  
-  for n=1, #score do
-    print(score[n])
-    love.graphics.draw(imgChiffres[score[n]], largeur - 14 - n * 11, hauteur - 41)
-  end
-  
 end
 
 function love.keypressed(key)
-  if key == "space" then
-    CreateTir("player", "tir_player", player.x, player.y - player.h / 2 - DECALAGE_TIR / 2, 0 , VITESSE_TIR_PLAYER)
-    if player.tir >= 2 then
-      CreateTir("player", "tir_player", player.x + DECALAGE_TIR, player.y - player.h / 2, 0, VITESSE_TIR_PLAYER)
-      CreateTir("player", "tir_player", player.x - DECALAGE_TIR, player.y - player.h / 2, 0, VITESSE_TIR_PLAYER)
-      if player.tir >= 3 then
-        CreateTir("player", "tir_playerD", player.x + DECALAGE_TIR * 3, player.y - player.h / 2, - VITESSE_TIR_PLAYER, VITESSE_TIR_PLAYER)
-        CreateTir("player", "tir_playerG", player.x - DECALAGE_TIR * 3, player.y - player.h / 2, VITESSE_TIR_PLAYER, VITESSE_TIR_PLAYER)
-        if player.tir >=4 then
-          CreateTir("player", "tir_player", player.x + DECALAGE_TIR * 2, player.y - player.h / 2 + DECALAGE_TIR / 2, 0, VITESSE_TIR_PLAYER)
-          CreateTir("player", "tir_player", player.x - DECALAGE_TIR * 2, player.y - player.h / 2 + DECALAGE_TIR / 2, 0, VITESSE_TIR_PLAYER)
+  if menu == "Accueil" then
+    if key == "space" then
+      menu = "Jeu"
+      DemarreJeu()
+    end
+  elseif menu == "Jeu" then
+    if key == "space" then
+      CreateTir("player", "tir_player", player.x, player.y - player.h / 2 - DECALAGE_TIR / 2, 0 , VITESSE_TIR_PLAYER)
+      if player.tir >= 2 then
+        CreateTir("player", "tir_player", player.x + DECALAGE_TIR, player.y - player.h / 2, 0, VITESSE_TIR_PLAYER)
+        CreateTir("player", "tir_player", player.x - DECALAGE_TIR, player.y - player.h / 2, 0, VITESSE_TIR_PLAYER)
+        if player.tir >= 3 then
+          CreateTir("player", "tir_playerD", player.x + DECALAGE_TIR * 3, player.y - player.h / 2, - VITESSE_TIR_PLAYER, VITESSE_TIR_PLAYER)
+          CreateTir("player", "tir_playerG", player.x - DECALAGE_TIR * 3, player.y - player.h / 2, VITESSE_TIR_PLAYER, VITESSE_TIR_PLAYER)
           if player.tir >=4 then
-            CreateTir("player", "tir_playerD", player.x + DECALAGE_TIR * 4, player.y - player.h / 2, - VITESSE_TIR_PLAYER, VITESSE_TIR_PLAYER)
-            CreateTir("player", "tir_playerG", player.x - DECALAGE_TIR * 4, player.y - player.h / 2, VITESSE_TIR_PLAYER, VITESSE_TIR_PLAYER)
+            CreateTir("player", "tir_player", player.x + DECALAGE_TIR * 2, player.y - player.h / 2 + DECALAGE_TIR / 2, 0, VITESSE_TIR_PLAYER)
+            CreateTir("player", "tir_player", player.x - DECALAGE_TIR * 2, player.y - player.h / 2 + DECALAGE_TIR / 2, 0, VITESSE_TIR_PLAYER)
+            if player.tir >=4 then
+              CreateTir("player", "tir_playerD", player.x + DECALAGE_TIR * 4, player.y - player.h / 2, - VITESSE_TIR_PLAYER, VITESSE_TIR_PLAYER)
+              CreateTir("player", "tir_playerG", player.x - DECALAGE_TIR * 4, player.y - player.h / 2, VITESSE_TIR_PLAYER, VITESSE_TIR_PLAYER)
+            end
           end
         end
       end
+    end
+  elseif menu == "Game Over" then
+    if key == "return" then
+      menu = "Accueil"
     end
   end
 end
